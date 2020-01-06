65641b20b27e1888761288311fc463c80be58605
RodrigoMattosoSilveira <r3850365@gmail.com>
2020-01-06 11:47:18 -0800

    FM-843-Two-Stage-Dockerfile-branch  -  A surefooted way to extract the version from package.json

--- .circleci/validate_version.sh
+++ .circleci/validate_version.sh
@@ -2,10 +2,17 @@
 
 set -ev
 echo "Executing validate_version"
-package_version=`npx -c 'echo "$npm_package_version"'`
+
+package_version=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
 echo "package_version = "$package_version
+
 changelog_version=`sed -nE 's/## \[([0-9]+(\.[0-9]+)+)\]( +- +[0-9]+-[0-9]+-[0-9]+)?/\1/p' CHANGELOG.md | head -n 1`
 echo "changelog_version = "$changelog_version
+
 if [[ ${package_version} != "${changelog_version}" ]]; then
     echo "🚫 Versions in package.json (${package_version}) and CHANGELOG.md (${changelog_version}) are different"
     exit 1
