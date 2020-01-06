100c988319cb67b3a1cfa9ba51ad7e50501254d6
RodrigoMattosoSilveira <r3850365@gmail.com>
2020-01-06 14:18:51 -0800

    FM-843-Two-Stage-Dockerfile-branch  -  Added logic to run version validation logic only for pull requests

--- .circleci/validate_version.sh
+++ .circleci/validate_version.sh
@@ -3,23 +3,27 @@
 set -ev
 echo "Executing validate_version"
 
-echo "Pull Request = "$CI_PULL_REQUEST
-package_version=$(cat package.json \
-  | grep version \
-  | head -1 \
-  | awk -F: '{ print $2 }' \
-  | sed 's/[",]//g')
-echo "package_version = "$package_version
+if [[ ! -z $CI_PULL_REQUEST ]] ; then
+  echo "This is a pull request build; validate changelog and package.json versions"
+  package_version=$(cat package.json \
+    | grep version \
+    | head -1 \
+    | awk -F: '{ print $2 }' \
+    | sed 's/[",]//g')
+  echo "package_version = "$package_version
 
-changelog_version=`sed -nE 's/## \[([0-9]+(\.[0-9]+)+)\]( +- +[0-9]+-[0-9]+-[0-9]+)?/\1/p' CHANGELOG.md | head -n 1`
-echo "changelog_version = "$changelog_version
+  changelog_version=`sed -nE 's/## \[([0-9]+(\.[0-9]+)+)\]( +- +[0-9]+-[0-9]+-[0-9]+)?/\1/p' CHANGELOG.md | head -n 1`
+  echo "changelog_version = "$changelog_version
 
-if [[ ${package_version} != "${changelog_version}" ]]; then
-    echo "🚫 Versions in package.json (${package_version}) and CHANGELOG.md (${changelog_version}) are different"
-    exit 1
+  if [[ ${package_version} != "${changelog_version}" ]]; then
+      echo "🚫 Versions in package.json (${package_version}) and CHANGELOG.md (${changelog_version}) are different"
+      exit 1
+  fi
+  if git ls-remote -q --tags | grep -q "v${package_version}$"; then
+      echo "🚫 Tag already exists for version ${package_version}"
+      exit 2
+  fi
+  echo "✅ Pull request introduces version ${package_version}"
+elif
+  echo "✅ Not a pull request build"
 fi
-if git ls-remote -q --tags | grep -q "v${package_version}$"; then
-    echo "🚫 Tag already exists for version ${package_version}"
-    exit 2
-fi
-echo "✅ Pull request introduces version ${package_version}"
