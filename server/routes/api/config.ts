693ac4b27acbecc4be69f583a2d7fbee3d5b9243
dna <denis.antonioli@weichaiamerica.com>
2020-01-07 17:29:15 +0100

    FM-843: align with common usage, remove reference to CONFIG_FILE; do not share file between server/ and src/

--- server/routes/api/config.ts
+++ server/routes/api/config.ts
@@ -1,19 +1,12 @@
 // CRUD: Simple Create, Read, Update, Delete api operations for data management.
 
 import { Router } from 'express';
-const request = require('request');
-const path = require('path');
-const fs = require('fs');
 
 import log from '../../helpers/bunyan';
-import appConfig from '../../helpers/config';
 import { Results } from '../../models/results';
-import Config from '../../models/config';
-import { appDefaultConfig } from '../../../src/app/_internal/configuration/configuration';
-
+import LocalConfigLib from '../../helpers/localConfig';
 
 const modelName = 'config'; // This needs to exactly match the corresponding file name under /server/models/, minus the extension
-const endpointUrl = appConfig.api_domain + '/' + appConfig.api_path + '/' + appConfig.api_selfTenant;
 const crudRouter = Router();
 
 
@@ -25,11 +18,10 @@ crudRouter.route('/' + modelName)
 .get((req: any, res: any, next: any) => {
 
     const results: Results = {};
-    const config: Config = appDefaultConfig; // By default the config is set to match that from /src/app/_internal/configuration/configuration
 
     results.info = 'Retrieved ' + modelName + ' from BFF';
     results.success = true;
-    results.data = config;
+    results.data =  LocalConfigLib.getAppDefaultConfig();
 
     return res.json(results);
 });
